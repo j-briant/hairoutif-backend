7d0360ef43500373357b6f3ee4dfb1efd70af1ca
j-briant <jbriant@DESKTOP-CO9HEQS.localdomain>
2021-04-09 17:56:03 +0200

    fck go back

--- server/database.js
+++ server/database.js
@@ -2,18 +2,15 @@
 
 const postgres = require('pg')
 const log = require('./logger')
+const connectionString = process.env.DATABASE_URL
 
 // Initialize postgres client
-const client = new postgres.Client(
-	connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-);
+const client = new postgres.Client({ connectionString })
 
 // Connect to the DB
 client.connect().then(() => {
 	log.info(`Connection string: ${connectionString}`)
+	log.info(`Client: ${client}`)
 	log.info(`Connected to ${client.database} at ${client.host}: ${client.port}`)
 }).catch(log.error)
 
